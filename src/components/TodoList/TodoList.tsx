827bec0c1d1aed1597e40f8e438b13cb7a523c96
PiekharievaK <pechareva@gmail.com>
2025-03-26 02:52:56 +0200

    github test check2

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { TodoItem } from '../TodoItem/TodoItem';
+import { TodoItem } from '../TodoItem/TodoItem';
 import { Todo } from '../../types/Todo';
 
 type Props = {
@@ -29,47 +29,12 @@ export const TodoList: React.FC<Props> = ({
 
     <tbody>
       {todos.map(item => (
-        <tr data-cy="todo" className="has-background-info-light" key={item.id}>
-          <td className="is-vcentered">{item.id}</td>
-          <td className="is-vcentered">
-            {item.completed && (
-              <span className="icon" data-cy="iconCompleted">
-                <i className="fas fa-check" />
-              </span>
-            )}
-          </td>
-          <td className="is-vcentered is-expanded">
-            <p
-              className={
-                item.completed ? 'has-text-success' : 'has-text-danger'
-              }
-            >
-              {item.title}
-            </p>
-          </td>
-          <td className="has-text-right is-vcentered">
-            <button
-              data-cy="selectButton"
-              className="button"
-              type="button"
-              onClick={() => setSelectedId(item.id)}
-            >
-              <span className="icon">
-                <i
-                  className={
-                    selectedId === item.id ? 'far fa-eye-slash' : 'far fa-eye'
-                  }
-                />
-              </span>
-            </button>
-          </td>
-        </tr>
-        // <TodoItem
-        //   todo={item}
-        //   key={item.id}
-        //   selectedId={selectedId}
-        //   setSelectedId={setSelectedId}
-        // />
+        <TodoItem
+          todo={item}
+          key={item.id}
+          selectedId={selectedId}
+          setSelectedId={setSelectedId}
+        />
       ))}
     </tbody>
   </table>
